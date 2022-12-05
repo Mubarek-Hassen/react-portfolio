cc81b1b446a288b292673e8b83fd1913974ac800
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-12-05 06:16:56 -0500

    Working on layout and basic info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Header from './components/Header';
+import Intro from './components/Body/Intro';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer =()=>{
+  return <section>
+    <a href="">E-Mail</a>
+    <br />
+    <a href="">LinkedIn</a>
+    <br />
+    <a href="">GitHub</a>
+  </section>
+}
+
+export default Footer;
