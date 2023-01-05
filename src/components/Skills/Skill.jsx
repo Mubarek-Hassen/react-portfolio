f8fade16c9519806ad7ca07b3d0ac0e1df18cfd8
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-01-04 23:34:35 -0500

    Working on skills description and experience.

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -1,13 +1,12 @@
 import { Box, Typography, CardActionArea } from "@mui/material"
 
 const Skill =(props)=>{
-  const colorsChoice = ['rgba(0,0,200,0.3)', 'navajowhite', 'darkgreen', 'goldenrod', 'chocolate', 'wheat', 'tomato', 'peru', 'tan', 'slateblue', 'sienna', 'purple']
-  let colorPicked = colorsChoice[Math.floor(Math.random()*colorsChoice.length)]
-
-  return <Box sx={{  m: 1, border: '4px solid', p: 1, textAlign: 'center', backgroundColor: colorPicked, }} >
+  const colorsChoice = [ 'skyblue','lightblue', 'salmon', 'rosybrown', 'navajowhite', 'darkolivegreen', 'goldenrod', 'chocolate', 'wheat', 'tomato', 'peru', 'tan', 'slateblue', 'sienna', 'purple'];
+  let colorPicked = colorsChoice[Math.floor(Math.random()*colorsChoice.length)];
+  return <Box sx={{  m: 1, border: '4px solid', p: 1, textAlign: 'center', backgroundColor: colorPicked, }}>
     <CardActionArea>
-    <img src={props.image} alt={props.alt} style={{ width: '14rem', height: '10rem', border: '4px black solid', backgroundColor: 'ghostwhite', padding: '2px'}}  />
-    <Typography variant="h5" sx={{backgroundColor: 'rgba(0,0,0,1)', color: 'ghostwhite'}}>{props.name}</Typography>
+    <img src={props.image} alt={props.alt} style={{ width: '15rem', height: '12rem', border: '4px black solid', backgroundColor: 'ghostwhite' , padding: '2px'}}  />
+    <Typography variant="h4" sx={{backgroundColor: 'rgba(0,0,0,0.7)', color: 'ghostwhite', border: 'black 4px solid'}}>{props.name}</Typography>
     </CardActionArea>
   </Box>
 
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,14 @@
 import Skill from "./Skill"
 import objOfSkills from '../../data/Skills.json'
-import { Box } from "@mui/material"
+import { Box, Typography, createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material"
 const Skills =()=>{
-
-  return <Box sx={{ flexGrow: 1, py: 10, px: 4, display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', alignContent: 'center', }}>
-  { objOfSkills.map(( item, i) => <Skill key={i} image={item.image} alt={item.name} name={item.name} />)}
+  let theme = createTheme();
+  theme = responsiveFontSizes(theme);
+  return <Box sx={{ flexGrow: 1, py: 10, px: 2, textAlign: 'center' }}>
+    <Typography variant="h2" sx={{ fontWeight: '500'}}> Skills </Typography>
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', alignContent: 'center', }}>
+    { objOfSkills.map(( item, i) => <Skill key={i} image={item.image} alt={item.name} name={item.name} />)}
+    </Box>
   </Box>
 }
 
