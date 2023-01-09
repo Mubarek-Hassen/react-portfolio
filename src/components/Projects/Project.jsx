74138e035ad333ea8f4c61ef4f7d4a408ade996e
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-01-09 17:36:58 -0500

    Added framer motion

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -2,9 +2,15 @@ import { Box } from "@mui/material"
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea } from '@mui/material';
+import { motion } from "framer-motion";
+
 
 const Project =(props)=>{
-  return <Box sx={{textAlign: 'center', border: 'black 1px solid', my: 2}}>
+  return <Box component={motion.div} sx={{textAlign: 'center', border: 'black 1px solid', my: 2, p: 1 }}
+    whileHover={{ scale: 1.1, borderRadius: '5%'}} 
+    transition={{ duration: 0.3 }} 
+    whileTap={{ scale: 0.8}}
+    >
       <CardActionArea>
         <img src={props.image} width='250px' height='150px' alt="" />
         <Box sx={{width: '18rem', textAlign: 'center', height: '8rem'}} >
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,16 @@
 import { Box, Typography, Divider, createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material";
 import Project from "./Project";
 import data from "../../data/Projects.json"
-
+import { motion } from "framer-motion";
 const Projects =()=>{
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
 
-  return <Box sx={{flexGrow: 1, py: 10, px: 2, textAlign: 'center'  }}>
+  return <Box component={motion.div} initial={{ opacity: 0}} transition={{ duration: 0.5}} animate={{opacity: 1}} sx={{flexGrow: 1, py: 10, px: 2, textAlign: 'center'  }}>
     <ThemeProvider theme={theme}>
     <Typography variant="h2" sx={{m: 3, fontWeight: '500'}}>Projects</Typography>
     <Typography variant="h5">
-      The General Assembly Coding Bootcamp was a great journey were we learned and implemented what was taught daily. A lot of our work ressembled and mimicked a day to day work of a developer.
+      The General Assembly Coding Bootcamp was a great journey were we learned and implemented what was taught daily. A lot of our work ressembled and mimicked a day to day work of a developer. 
     </Typography>
     
     <Divider sx={{mb: '3rem', backgroundColor: 'green', mx: 10 }}/>
