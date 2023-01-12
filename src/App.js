5e2117c1c9babc750ae5793801f6300aef03cf31
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-01-12 17:06:47 -0500

    icon changed, setup for deployment next

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import React, { Suspense } from 'react';
 import MiniDrawer from './components/UI/MiniDrawer';
-import About from './components/About';
 import Home from './components/Home';
 import { Routes, Route } from 'react-router-dom';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
 
+const Skills = React.lazy(()=> import('./components/Skills/Skills'));
+const Projects = React.lazy(()=> import('./components/Projects/Projects'));
+const About = React.lazy(()=> import('./components/About'));
 
 function App() {
   return (
     <section>
       <MiniDrawer>
+        <Suspense fallback={<p>Loading...</p>} >
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/skills' element={<Skills />} />
           <Route path='/projects' element={<Projects />} />
         </Routes>
+        </Suspense>
       
       </MiniDrawer>
       </section>
